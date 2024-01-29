3301db39b9a1d6a1d2983d62927270c61d1ea8ea
Aliwangara <aliwangara63@gmail.com>
2024-01-29 20:23:00 +0300

    created a database where I could update information and it displays on my website

--- Personal_Portfolio_Project/urls.py
+++ Personal_Portfolio_Project/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 import main
 from main import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,9 +28,10 @@ urlpatterns = [
     path('about', views.about, name='About'),
     path('services', views.services, name='services'),
     path('contact', views.contact, name='contact'),
+    path('blog', views.handleblog, name = 'handleblog'),
 
     path('SignUp/', views.signup, name='SignUp'),
     path('LogIn/', views.handleLogIn, name='handleLogIn'),
     path('LogOut/', views.handleLogOut, name='handleLogOut'),
 
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- main/views.py
+++ main/views.py
@@ -1,4 +1,11 @@
-from django.shortcuts import render
+from _ast import Pass
+from email import message
+
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate,login,logout
+from main.models import Contacts, Blogs
 
 
 # Create your views here.
@@ -15,26 +22,67 @@ def services(request):
 
 
 def contact(request):
+    if request.method=="POST":
+        fname = request.POST.get("name")
+        femail = request.POST.get("email")
+        fPhoneno= request.POST.get("num")
+        fdescription= request.POST.get("desc")
+        query = Contacts(name = fname, email = femail, phonenumber = fPhoneno, description = fdescription)
+        query.save()
+        messages.success(request, "Thanks for contacting. I will get back to you soon!")
+        return redirect('/contact')
+
     return render(request, 'contact.html')
+
+def handleblog(request):
+    posts = Blogs.objects.all()
+    context= {'posts':posts}
+    return render(request, 'handleblog.html', context)
 
 
 def handleLogIn(request):
+    if request.method == "POST":
+        get_email = request.POST.get('email')
+        get_password = request.POST.get('pass1')
+        myuser = authenticate (username = get_email, password = get_password)
+        if myuser is not None:
+            login(request, myuser)
+            messages.success(request, "Login Success")
+            return redirect('/')
+        else:
+            messages.error(request,"Invalid Credentials")
+
     return render(request, 'LogIn.html')
 
 
 def handleLogOut(request):
-    return render(request, 'LogOut.html')
+    logout(request)
+    messages.success(request,"Logout success")
+    return render(request, 'LogIn.html')
 
 
-def signup(request):
-    if request.method == "POST" :
+def signup(request, ):
+    if request.method == "POST":
         get_email = request.POST.get('email')
         get_password = request.POST.get('pass1')
         get_confirm_password = request.POST.get('pass2')
+        if get_password != get_confirm_password:
+            messages.info(request, 'Password not matching')
+            return redirect('/SignUp/')
 
-        print(get_email)
-        print(get_password)
-        print(get_confirm_password)
+
+        try:
+            if User.objects.get(username=get_email):
+                messages.warning(request, "Email is already Taken")
+                return redirect('/SignUp/')
+
+        except Exception as identifier:
+            Pass
+
+        myuser = User.objects.create_user(get_email, get_email, get_password)
+        myuser.save()
+        messages.success(request, "user created please login")
+        return redirect('/LogIn/')
+    return render(request, "signup.html")
 
 
-    return render(request, 'signup.html'),
